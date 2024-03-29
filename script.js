939b8cf1f14f623a76e196298b826ab8faf80ced
YassineOularbi <yassineoularbi4@gmail.com>
2024-03-29 11:52:49 +0000

    Add function search

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
 const apiKey = '18002b8e2a0e5dcc0d2aeda8d5c816e7';
-const city = 'khenifra';
-let weatherdt ={};
-let weatherdtSearch ={};
-
-    async function weatherData() {
-      let weatherData;
-
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`)
-
-      weatherData = await res.json();
-        if(weatherData){
-        weatherdt = weatherData;
-        }
-
-    }
-
-weatherData();
-setTimeout(function(){
-console.log(weatherdt)
-
-document.getElementById('city-Name').textContent = weatherdt.name;
-document.getElementById('latitude').textContent = 'Latitude : ' +weatherdt.coord.lat;
-document.getElementById('longitude').textContent = 'Longitude : ' +weatherdt.coord.lat;
-document.getElementById('temperature').textContent = weatherdt.main.temp+ ' °C';
-document.getElementById('high-temp').textContent = ' High : ' +weatherdt.main.temp_max+ ' °C';
-document.getElementById('low-temp').textContent = ' Low : ' +weatherdt.main.temp_min+ ' °C';
-
-
-},1000)
-
-/*const key = "244155d43b7fc901f0f438ae76092427";
 
 async function searchWeather() {
-    let villeInput = 'khenifra';
+    let villeInput = document.getElementById("search").value.trim();
 
     if (villeInput !== '') {
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${villeInput}&limit=5&appid=${key}`);
-        const data = await response.json();
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${villeInput}&appid=${apiKey}&units=metric&lang=en`)
 
-        if (data.length > 0) {
-            const { lat, lon } = data[0];
-            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-            const weatherResponse = await fetch(weatherUrl);
-            const weatherData = await weatherResponse.json();
+        const weatherData = await response.json();
+        Display(weatherData);
 
-            console.log(weatherData);
-        } else {
-            alert('City not found.');
-        }
     } else {
         alert('Please enter a city name.');
     }
 }
-*/
+
+   function Display(weatherdt){
+    document.getElementById('city-Name').textContent = weatherdt.name;
+    document.getElementById('latitude').textContent = 'Latitude : ' +weatherdt.coord.lat;
+    document.getElementById('longitude').textContent = 'Longitude : ' +weatherdt.coord.lon;
+    document.getElementById('temperature').textContent = weatherdt.main.temp+ ' °C';
+    document.getElementById('high-temp').textContent = ' High : ' +weatherdt.main.temp_max+ ' °C';
+    document.getElementById('low-temp').textContent = ' Low : ' +weatherdt.main.temp_min+ ' °C';
+    document.getElementById('wind-speed').textContent = weatherdt.wind.speed;
+    document.getElementById('wind-speed-sudden').textContent = weatherdt.wind.speed;
+    document.getElementById('rainfall').textContent = weatherdt.rain["1h"];
+    document.getElementById('humidity').textContent = weatherdt.main.humidity+'%';
+    document.getElementById('sunrise').textContent = convertirHorodatageUnix(weatherdt.sys.sunrise);
+    document.getElementById('sunset').textContent = convertirHorodatageUnix(weatherdt.sys.sunset);
+    
+    }
+
+
+    function convertirHorodatageUnix(heureUnix) {
+    
+        const milliseconds = heureUnix * 1000;
+    
+        
+        const dateUTC = new Date(milliseconds);
+    
+        const dateLocale = new Date(dateUTC.getTime());
+    
+     
+        const heure = ('0' + dateLocale.getHours()).slice(-2);
+        const minutes = ('0' + dateLocale.getMinutes()).slice(-2);
+        const secondes = ('0' + dateLocale.getSeconds()).slice(-2);
+    
+        return heure + ':' + minutes + ':' + secondes;
+    }
